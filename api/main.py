13f6e366c9bb4c5e92bf80205558aa721dbda372
Daniel Silva <daniel.silva@wikki.com.br>
2024-02-22 21:00:42 -0300

    Salvando frame

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+import os
+import cv2
 from datetime import datetime
 from multiprocessing import Process
 from api import crud
@@ -7,9 +9,7 @@ from core.detection import LightningDetect
 ws = ConnectionManager.instance()
 
 
-async def save_data_func(detection_id, centroid_x, centroid_y, size):
-    now = datetime.now()
-    timestamp = datetime.timestamp(now)
+async def save_data_func(detection_id, centroid_x, centroid_y, size, timestamp):
     crud.create_lightning(timestamp, size, centroid_x, centroid_y, detection_id)
     await ws.broadcast(
         {
@@ -21,15 +21,26 @@ async def save_data_func(detection_id, centroid_x, centroid_y, size):
     )
 
 
-def save_frame_func(frame):
-    pass
+def save_frame_func(detection_id, frame, timestamp):
+    dir_name = f"detection_{detection_id}"
+
+    home_dir = os.path.expanduser("~")
+    dir_path = os.path.join(home_dir, dir_name)
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    filename = f"frame_{timestamp}.jpg"
+    cv2.imwrite(os.path.join(dir_path, filename), frame)
 
 
 async def listen_pipe(process: Process, pipe):
     while process.is_alive():
+        now = datetime.now()
+        timestamp = datetime.timestamp(now)
         detection_id, cx, cy, size, frame = pipe.recv()
-        await save_data_func(detection_id, cx, cy, size)
-        # await save_frame_func(frame)
+        await save_data_func(detection_id, cx, cy, size, timestamp)
+        save_frame_func(detection_id, frame, timestamp)
     pipe.close()
 
 
@@ -39,6 +50,3 @@ def start_detection(pipe):
     detection_id = crud.create_detection_record(start_time)
     detect = LightningDetect(detection_id, pipe)
     detect.detecting_process()
-
-    # Optionally wait for process to finish before returning
-    # process.join()
