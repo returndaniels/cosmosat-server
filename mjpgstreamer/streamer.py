e637f98ec6c1473dc3a5229cb111b2656fc4af81
Daniel Silva <daniel.silva@wikki.com.br>
2024-02-23 02:56:02 -0300

    feat: stream fica em execução enquanto o processo pai estiver ativo

--- mjpgstreamer/streamer.py
+++ mjpgstreamer/streamer.py
@@ -1,7 +1,8 @@
+from multiprocessing import Process
 import subprocess
 
 
-def start_stream():
+def start_stream(parent_process: Process):
     # Inicie o mjpg-streamer em uma subprocesso
     process = subprocess.Popen(
         [
@@ -13,8 +14,8 @@ def start_stream():
         ]
     )
 
-    # Mantenha o processo em execução enquanto o script estiver ativo
-    while True:
+    # Mantenha o processo em execução enquanto o processo pai estiver ativo
+    while parent_process.is_alive():
         # Verifique se o processo ainda está em execução
         if process.poll() is not None:
             # O processo terminou, então pare o loop
